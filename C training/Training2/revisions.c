80607972e9802466033dea546ee539187ee02cd5
Théo Tchilinguirian <60852187+theotchlx@users.noreply.github.com>
2023-01-25 22:30:51 +0100

    Update and rename ENTR(revision).c to revisions.c

--- C training/Training2/revisions.c
+++ C training/Training2/revisions.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "mdl.h"
+
+struct complexe {
+    int re;
+    int im;
+};
+
+typedef struct complexe complexe;
 
 
 /* Déclaration des fonctions */
